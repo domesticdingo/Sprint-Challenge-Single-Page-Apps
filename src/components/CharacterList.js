3e5b794ca03022e70b793244e66bfd233f29c15e
Indigo Richards <thedomesticdingo@gmail.com>
2019-11-15 09:35:52 -0800

    MVP

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,66 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+const CharacterList = props => {
+  const [characters, setCharacters] = useState ([])
+  const [query, setQuery] = useState ("")
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    const getCharacters = () => {
+      axios
+        .get('https://rickandmortyapi.com/api/character/')
+        .then(response => {
+          const data = response.data.results.filter(param =>
+            param.name.toLowerCase().includes(query.toLowerCase()))
+            setCharacters(data)
+        })
+        .catch(error => {
+          console.error('Server error: ', error)
+        })
+    }
+
+    getCharacters();
+  }, [query]);
+
+  const handleInputChange = event => {
+    setQuery(event.target.value)
+  }
 
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-  );
+    <div>
+      <form className = "search">
+        <input
+          type="text"
+          onChange={handleInputChange}
+          value={query}
+          name="name"
+          tabIndex="0"
+          className="promp search-name"
+          placeholder="Search by character name"
+          autoComplete="off"
+          />
+      </form>
+      <div>
+      {characters.map(char => (
+        <CharacterCard key={char.id} char={char} />
+      ))}
+      </div>
+    </div>
+  )
 }
+
+
+function CharacterCard({ char }) {
+  const {image, name, status, species} = char;
+  return (
+    <div className="character-card">
+      <img src={image} alt="A character's face" />
+      <h2>{name}</h2>
+      <div className="character-info">
+        <p>Status: {status}</p>
+        <p>Species: {species}</p>
+      </div>
+    </div>
+  )
+}
+export default CharacterList
